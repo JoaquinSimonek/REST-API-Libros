83aec8b738210e9165defbe565b555c6af5b16f1
joaquinsimonek <joaquinsimonek@gmail.com>
2023-10-18 12:46:51 -0300

    Obtener todos los libros y obtener por ID

---
+++ src/controller.js
@@ -0,0 +1,28 @@
+import {pool} from './database.js';
+
+class LibrosController{
+
+    async getAll(req, res) {
+        const [result] = await pool.query('SELECT * FROM libros');
+        res.json(result);
+    }
+
+    async getOne(req, res) {
+        try{
+                const libro = req.body;
+                const id_libro = parseInt(libro.id);
+                const [result] = await pool.query(`select * from libros where id=?`, [id_libro]);
+                if (result[0]!=undefined){
+                    res.json(result);
+                }else{
+                    res.json({"Error": "No se ha encontrado un libro con la Id especificada"});
+                }
+            }catch(e) {
+                console.log(e);
+        }  
+    }
+
+
+}
+
+export const libro = new LibrosController();
