b629a3ce124a1d277ab5084328c7925ccc03ae22
joaquinsimonek <joaquinsimonek@gmail.com>
2023-10-23 20:16:50 -0300

    Se agrego eliminar libro y mensajes de error

--- src/controller.js
+++ src/controller.js
@@ -23,11 +23,47 @@ class LibrosController{
     }
 
     async add(req, res) {
-        const libro = req.body;
-        const [result] = await pool.query(`INSERT INTO libros(nombre, autor, categoria, año_publicacion, ISBN) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN]);
-        res.json({"Id insertado": result.insertId});
+        try {
+            const libro = req.body;
+            if (!libro.nombre || !libro.autor || !libro.categoria || !libro.año_publicacion || !libro.ISBN) {
+                res.status(400).json({ error: "Todos los campos deben estar completos." });
+            } else {
+
+                const [checkResult] = await pool.query("SELECT id FROM libros WHERE nombre=? AND autor=?", [libro.nombre, libro.autor]);
+                if (checkResult[0]) {
+                    res.status(409).json({ Error: `Ya existe un libro con el mismo nombre y autor.` });
+                } else {
+                    const [result] = await pool.query(`INSERT INTO libros(nombre, autor, categoria, año_publicacion, ISBN) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN]);
+                    res.json({"Id insertado": result.insertId});
+                }
+            }
+        } catch (e) {
+            console.log(e);
+            res.status(500).json({ Error: "Hubo un fallo al crear el libro." });
+            
+        }
     }
 
+    async delete(req, res) {
+        try{
+                const libro = req.body;
+                const isbn_libro = libro.ISBN;
+                
+                const [checkResult] = await pool.query("SELECT id FROM libros WHERE ISBN=?", [isbn_libro]);
+                
+                if (checkResult[0]) {
+                    
+                    await pool.query(`DELETE FROM libros where ISBN=?`, [isbn_libro]);
+                    res.json({"Aviso":"El libro ha sido eliminado."});
+                }else{
+                    res.status(404).json({ "Error": "No se ha encontrado un libro con el ISBN especificado." });
+                    
+                }
+            }catch(e) {
+                console.log(e);
+                res.status(500).json({"Error":"Hubo un fallo al eliminar el libro." });
+        }   
+    }
 
 }
 
--- src/routes.js
+++ src/routes.js
@@ -6,3 +6,4 @@ export const router = Router()
 router.get('/libros', libro.getAll);
 router.get('/libro', libro.getOne);
 router.post('/libronew', libro.add);
+router.delete('/librodel', libro.delete);
