70652f7d9c6c24afe04b83eb9547a0b501e87555
joaquinsimonek <joaquinsimonek@gmail.com>
2023-10-23 18:27:33 -0300

    Agregado alta de libros

--- src/controller.js
+++ src/controller.js
@@ -22,6 +22,12 @@ class LibrosController{
         }  
     }
 
+    async add(req, res) {
+        const libro = req.body;
+        const [result] = await pool.query(`INSERT INTO libros(nombre, autor, categoria, año_publicacion, ISBN) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN]);
+        res.json({"Id insertado": result.insertId});
+    }
+
 
 }
 
